f78b3c5bc464c90fdbeb2c0517f8c89c905f5f4f
森田賢二 <kenjimoritata@gmail.com>
2017-03-06 20:28:45 +0900

    interface1

---
+++ interface1/index.js
@@ -0,0 +1,11 @@
+var jane = {
+    firstName: "シェーン",
+    lastName: "オールマン",
+    age: 18
+};
+var mike = {
+    age: 25,
+    lastName: "シンクレア",
+    firstName: "マイケル"
+};
+document.body.innerHTML = jane.firstName + mike.age;
