27d996323b9ffda5f10faae0725eed17587dd78b
森田賢二 <kenjimoritata@gmail.com>
2017-03-06 20:01:47 +0900

    add any

---
+++ any/index.js
@@ -0,0 +1,9 @@
+var showhere;
+showhere = document.body;
+var stringlike;
+stringlike = "fafa";
+//objctはダメ
+if (stringlike.length) {
+    console.log("faf");
+}
+showhere.innerHTML = "JavaScriptのオブジェクトにはany";
